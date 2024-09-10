ae4aef4250e136f5ec7d1f2b656240c5ba7f1b14
Jssc-ps <jscpsilva@gmail.com>
2024-09-10 10:12:17 -0300

    lista05_1

---
+++ lista05_1.py
@@ -0,0 +1,5 @@
+valor_X = int(input('digite o número de passos para o norte: '))
+valor_Y = int(input('digite o número de passos para o leste (Y): '))
+
+calcular_distancia = valor_X + valor_Y
+print(f'distancia de : {calcular_distancia}')
