81e7352c312eef17a14aa0b93c0b63aec3b8f867
Jssc-ps <jscpsilva@gmail.com>
2024-09-10 10:15:21 -0300

    lista05_4

---
+++ lista05_4.py
@@ -0,0 +1,5 @@
+moedas_1_real = int(input('quantidade de moedas de 1 real: '))
+moedas_50_centados = int(input('quantidade de moedas de 50 centavos: '))
+moedas_25_centavos = int(input('quantidade de moedas de 25 centavos: '))
+
+total = (moedas_1_real *1) + (moedas_50_centados *0.5) + (moedas_25_centavos *0.25)
