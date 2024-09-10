1a447bbe47b858caf39ccc423ad589043c1bb78a
Jssc-ps <jscpsilva@gmail.com>
2024-09-10 10:15:07 -0300

    lista05_3

---
+++ lista05_3.py
@@ -0,0 +1,10 @@
+tipo_animal = input("Seu animal favorito é mamifero ou réptil?") .lower()
+
+if tipo_animal == "mamifero":
+    print("eu acho que seu animal favorito é um cachorro")
+
+elif tipo_animal == "réptil":
+    print("eu acho que seu animal favorito é uma tartaruga")
+
+else:
+    print("não consegui advinhar seu animal favorto.")
