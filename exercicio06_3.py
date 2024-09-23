7172c6c4c46f8618fd2d7be2668eff32e429353e
Jéssica Pereira <jscpsilva@gmail.com>
2024-09-23 20:40:39 -0300

    Atualizar o exercicio06_3.py
    comentário

--- exercicio06_3.py
+++ exercicio06_3.py
@@ -4,3 +4,6 @@ def tabuada (numero):
     for m in range (1, 11, 1):
         print(f'{nunero} X {m} = {numero*m}')
         sleep(1)
+if __name__ == '__main__':
+    número = int(input('Digite um número: '))
+    tabuada(numero)
